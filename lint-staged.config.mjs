0e91aa629e99369efcbf6d726e83db4a3d952655
Alex Parra <parraletz@gmail.com>
2025-04-26 20:46:06 -0600

    chore: added settings.yml

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,6 @@
 const config = {
   '**/*.{html,json,css,scss,md,mdx}': ['prettier -w'],
-  '**/*.{js,ts,jsx,tsx}': ['eslint --fix']
+  '**/*.{js,ts,jsx,tsx}': ['eslint --fix', 'pnpm test']
 }
 
 export default config
